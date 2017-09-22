0f4861b9587d71536df16221f121bb80c3e6e04e
kostya.savik@kandasoft.com <kostya.savik@kandasoft.com>
2017-09-22 22:20:47 +0300

    add project file

---
+++ stpProject/stpProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(stpProject.Startup))]
+namespace stpProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
